548708c34b99de2825ad7c4f74f6ff56957bc255
Parikshith Sharma <Parikshith Sharma>
2024-03-24 14:47:29 -0700

    Integration

--- diffuser.py
+++ diffuser.py
@@ -12,14 +12,16 @@ args = parser.parse_args()
 
 def run_sd(prompt):
     model_id = "runwayml/stable-diffusion-v1-5"
-    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16) # Use torch.float16 for mixed precision.
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32) # Use torch.float16 for mixed precision.
+    #pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
 
     image = pipe(prompt).images[0]
     #buffered = BytesIO()
     #image.save(buffered, format="JPEG")
     #img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-    
+    #return img_str
+
     return image
 
 if __name__ == "__main__":
